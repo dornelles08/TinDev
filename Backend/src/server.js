e1558ffe812b39bf73845e32de214f89589df6ea
Felipe Dornelles <lipe.dornelles@hotmail.com>
2020-04-12 23:26:04 -0300

    Backend finalizado

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const cors = require('cors')
 const mongoose = require('mongoose');
+
 const routes = require('./routes');
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const connectedUsers={};
+
+io.on('connection', socket =>{
+    const { user } = socket.handshake.query;
+    connectedUsers[user] = socket.id;
+});
 
 mongoose.connect('mongodb+srv://TinDev:TinDev@cluster0-sg3qs.mongodb.net/TinDev?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
